8742d0d04eb56a7be2f0a19a9d990bbd92bc3da9
Bryan Williams <bryan.williams@agilent.com>
2023-11-30 10:26:09 -0500

    NO_JIRA - added additional context comments in a few files
    - Updated template 'resources' files with some additional context and included the same comments in plugin's 'resources' files
    - Added comment to template springconfig
    - Added some comments to plugin configuration file to provide a little context on what it does
    - Updated basic-crud-actions readme to link to short explainer on using ConvertRecordsService

--- cookbook/plugins/direct-database-actions/basic-crud-actions/src/main/java/com/genohm/slims/custom/BasicCrudActionsConfiguration.java
+++ cookbook/plugins/direct-database-actions/basic-crud-actions/src/main/java/com/genohm/slims/custom/BasicCrudActionsConfiguration.java
@@ -4,6 +4,15 @@
 
 package com.genohm.slims.custom;
 
+/**
+ * <p>This class dictates the YAML configuration you can use to set the values of variables after uploading the plugin to your instance.</p>
+ *
+ * <p>Any property included will appear as a key in the YAML configuration, and when the plugin's routes are executed,
+ * the corresponding value in the YAML will be used as the current value of that property.</p>
+ *
+ * <p>Please use these configuration classes to avoid "magic numbers" and "magic strings" in your code to reference things that can be changed
+ * by end users in the SLIMS UI. You can reference the Development manual for further examples.</p>
+ */
 public class BasicCrudActionsConfiguration {
 
 	private String contentTypeDisplayValue;
